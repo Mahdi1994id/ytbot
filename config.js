6b4f3562d6425cc70832aee55cbc09220d323d52
Z User <z@container>
2026-08-09 20:51:05 +0000

    Hardcode RapidAPI key for simplicity

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const TOKEN = process.env.BOT_TOKEN;
 export const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-export const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+export const RAPIDAPI_KEY = 'e0af92aeffmshe646a2aaad89a7fp1ac404jsn52c5f203a4f9';
