2ced342473f03587cf0dc04f8f33a6070dced0be
Mahdi1994id <mahdi@local>
2026-08-28 14:42:04 +0330

    Remove hardcoded RapidAPI key from config

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-export const TOKEN = process.env.BOT_TOKEN;
+export const TOKEN = proces…OKEN;
 export const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-export const RAPIDAPI_KEY = 'e0af92aeffmshe646a2aaad89a7fp1ac404jsn52c5f203a4f9';
